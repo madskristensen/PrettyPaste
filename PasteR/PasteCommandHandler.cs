afdd9768a4112632c4587f808456a27e57faf75d
Mads Kristensen <post@madskristensen.net>
2013-03-21 23:25:55 -0700

    Fixed undo buffer functionality

--- PasteR/PasteCommandHandler.cs
+++ PasteR/PasteCommandHandler.cs
@@ -2,12 +2,10 @@
 using EnvDTE80;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
-using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.TextManager.Interop;
 using System;
 using System.Windows.Forms;
-using System.Windows.Threading;
 
 namespace PasteR
 {
@@ -24,7 +22,7 @@ namespace PasteR
         {
             _textView = textView;
             _dte = dte;
-            adapter.AddCommandFilter(this, out _nextCommandTarget);            
+            adapter.AddCommandFilter(this, out _nextCommandTarget);
         }
 
         public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
@@ -32,38 +30,40 @@ namespace PasteR
             if (pguidCmdGroup == _guid && nCmdID == _commandId)
             {
                 if (Clipboard.ContainsText())
-                {   
+                {
+                    string text = Clipboard.GetText(TextDataFormat.Text);
+
                     TextDocument doc = (TextDocument)_dte.ActiveDocument.Object("TextDocument");
-                    EditPoint start = doc.Selection.ActivePoint.CreateEditPoint();
-                    
-                    // Run the PasteR code after the default paste, so undo restores default
-                    Dispatcher.CurrentDispatcher.BeginInvoke(new Action(() =>
-                    {
-                        ReplaceText(doc, start, _textView.Caret.Position.BufferPosition);
-                    }), DispatcherPriority.Normal, null);
+                    EditPoint start = doc.Selection.TopPoint.CreateEditPoint();
+
+                    // First insert plain text
+                    doc.Selection.Insert(text);
+
+                    // Then replace with clean text, so undo restores the default behavior
+                    ReplaceText(doc, start, text);
+
+                    return VSConstants.S_OK;
                 }
             }
 
             return _nextCommandTarget.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
         }
 
-        private void ReplaceText(TextDocument doc, EditPoint start, int length)
+        private void ReplaceText(TextDocument doc, EditPoint start, string text)
         {
-            string text = Clipboard.GetText(TextDataFormat.Text);
             PasteCleaner cleaner = new PasteCleaner(text);
 
             if (cleaner.IsDirty())
             {
                 string clean = cleaner.Clean();
-
                 _dte.UndoContext.Open("Paste FixR");
 
                 // Insert
-                var edit = doc.CreateEditPoint(start);
-                edit.ReplaceText(length, clean, 0);
-                
+                start.Delete(text.Replace("\n", string.Empty).Length);
+                start.Insert(clean);
+
                 // Format
-                doc.Selection.MoveToPoint(edit, true);
+                doc.Selection.MoveToPoint(start, true);
                 FormatSelection();
                 _textView.Selection.Clear();
 
@@ -74,7 +74,7 @@ namespace PasteR
         private void FormatSelection()
         {
             Command command = _dte.Commands.Item("Edit.FormatSelection");
-            
+
             if (command.IsAvailable)
             {
                 _dte.ExecuteCommand("Edit.FormatSelection");
